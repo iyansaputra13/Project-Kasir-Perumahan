e398c23d832b5426aea20ed73d996d6f05c11cec
iyansaputra13 <ryansaputra20202@gmail.com>
2025-07-24 09:47:38 +0700

    fix database

--- model/transaksi_model.py
+++ model/transaksi_model.py
@@ -1,32 +1,32 @@
 # model/transaksi_model.py
-from config.db import get_connection
+from config.db_config import get_connection
+
+
 
 class TransaksiModel:
-    def __init__(self):
-        self.conn = get_connection()
-        self.cursor = self.conn.cursor()
-
     def simpan_transaksi(self, data):
+        conn = get_connection()
+        cursor = conn.cursor()
         query = """
             INSERT INTO transaksi (
-                nama, nik, tempat_lahir, tanggal_lahir, alamat, no_hp, email,
-                proyek, blok, tipe_rumah, harga_jual
-            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                nama, nik, ttl, alamat, no_hp, email,
+                proyek, blok, tipe, harga
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
-        self.cursor.execute(query, (
-            data['nama'], data['nik'], data['tempat_lahir'], data['tanggal_lahir'],
-            data['alamat'], data['no_hp'], data['email'],
-            data['proyek'], data['blok'], data['tipe_rumah'], data['harga_jual']
+        cursor.execute(query, (
+            data['nama'], data['nik'], data['ttl'], data['alamat'],
+            data['no_hp'], data['email'], data['proyek'], data['blok'],
+            data['tipe'], data['harga']
         ))
-        self.conn.commit()
+        conn.commit()
+        cursor.close()
+        conn.close()
 
     def ambil_semua_transaksi(self):
-        query = "SELECT * FROM transaksi"
-        self.cursor.execute(query)
-        return self.cursor.fetchall()
-
-    def __del__(self):
-        if hasattr(self, 'cursor'):
-            self.cursor.close()
-        if hasattr(self, 'conn'):
-            self.conn.close()
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM transaksi")
+        hasil = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return hasil
--- view/form_input_dialog.py
+++ view/form_input_dialog.py
@@ -1,91 +1,72 @@
-from PySide6.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QComboBox, QDateEdit, QHBoxLayout
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QComboBox, QDateEdit
 from PySide6.QtCore import QDate
-from model.transaksi_model import TransaksiModel
+from controller.transaksi_controller import TransaksiController
 
-transaksi_model = TransaksiModel()  # buat objek global hanya sekali
 
 class FormInputDialog(QDialog):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None):  # <-- diubah agar bisa menerima parent
         super().__init__(parent)
-        self.setWindowTitle("Input Transaksi")
-        self.setMinimumWidth(400)
+        self.setWindowTitle("Form Input Transaksi")
+        self.controller = TransaksiController()
+        self.setup_ui()
 
+    def setup_ui(self):
         layout = QVBoxLayout()
 
-        # ==== Data Pembeli ====
+        # Data Pembeli
         self.nama_input = QLineEdit()
         self.nik_input = QLineEdit()
-        self.tempat_lahir_input = QLineEdit()
-        self.tanggal_lahir_input = QDateEdit()
-        self.tanggal_lahir_input.setCalendarPopup(True)
-        self.tanggal_lahir_input.setDate(QDate.currentDate())
+        self.ttl_input = QLineEdit()
         self.alamat_input = QLineEdit()
-        self.no_hp_input = QLineEdit()
+        self.hp_input = QLineEdit()
         self.email_input = QLineEdit()
+
+        # Data Pemesanan
+        self.proyek_input = QLineEdit()
+        self.blok_input = QLineEdit()
+        self.tipe_input = QLineEdit()
+        self.harga_input = QLineEdit()
 
         layout.addWidget(QLabel("Nama"))
         layout.addWidget(self.nama_input)
         layout.addWidget(QLabel("NIK"))
         layout.addWidget(self.nik_input)
-        layout.addWidget(QLabel("Tempat Lahir"))
-        layout.addWidget(self.tempat_lahir_input)
-        layout.addWidget(QLabel("Tanggal Lahir"))
-        layout.addWidget(self.tanggal_lahir_input)
+        layout.addWidget(QLabel("Tempat/Tanggal Lahir"))
+        layout.addWidget(self.ttl_input)
         layout.addWidget(QLabel("Alamat"))
         layout.addWidget(self.alamat_input)
         layout.addWidget(QLabel("No. HP"))
-        layout.addWidget(self.no_hp_input)
+        layout.addWidget(self.hp_input)
         layout.addWidget(QLabel("Email"))
         layout.addWidget(self.email_input)
-
-        # ==== Data Pemesanan ====
-        self.proyek_input = QLineEdit()
-        self.blok_input = QLineEdit()
-        self.tipe_input = QComboBox()
-        self.tipe_input.addItems(["RUBY", "EMERALD", "SAFIR"])
-        self.harga_input = QLineEdit()
-        self.skema_input = QComboBox()
-        self.skema_input.addItems(["KPR", "Tunai Bertahap"])
-
         layout.addWidget(QLabel("Nama Proyek"))
         layout.addWidget(self.proyek_input)
         layout.addWidget(QLabel("Blok/Kavling"))
         layout.addWidget(self.blok_input)
         layout.addWidget(QLabel("Tipe Rumah"))
         layout.addWidget(self.tipe_input)
-        layout.addWidget(QLabel("Harga Jual (Rp)"))
+        layout.addWidget(QLabel("Harga Jual (Tanah & Bangunan)"))
         layout.addWidget(self.harga_input)
-        layout.addWidget(QLabel("Skema Pembayaran"))
-        layout.addWidget(self.skema_input)
 
-        # Tombol Simpan
-        btn_layout = QHBoxLayout()
-        simpan_btn = QPushButton("Simpan")
-        simpan_btn.clicked.connect(self.simpan)
-        batal_btn = QPushButton("Batal")
-        batal_btn.clicked.connect(self.reject)
-        btn_layout.addWidget(simpan_btn)
-        btn_layout.addWidget(batal_btn)
+        simpan_button = QPushButton("Simpan Transaksi")
+        simpan_button.clicked.connect(self.simpan_data)
+        layout.addWidget(simpan_button)
 
-        layout.addLayout(btn_layout)
         self.setLayout(layout)
 
-    def simpan(self):
+    def simpan_data(self):
         data = {
             "nama": self.nama_input.text(),
             "nik": self.nik_input.text(),
-            "tempat_lahir": self.tempat_lahir_input.text(),
-            "tanggal_lahir": self.tanggal_lahir_input.date().toString("yyyy-MM-dd"),
+            "ttl": self.ttl_input.text(),
             "alamat": self.alamat_input.text(),
-            "no_hp": self.no_hp_input.text(),
+            "no_hp": self.hp_input.text(),  # <- sudah benar
             "email": self.email_input.text(),
             "proyek": self.proyek_input.text(),
             "blok": self.blok_input.text(),
-            "tipe": self.tipe_input.currentText(),
-            "harga": self.harga_input.text(),
-            "skema": self.skema_input.currentText()
+            "tipe": self.tipe_input.text(),
+            "harga": self.harga_input.text()
         }
 
-        # Simpan ke database
-        transaksi_model.simpan_transaksi(data)
+        self.controller.simpan_transaksi(data)
         self.accept()
